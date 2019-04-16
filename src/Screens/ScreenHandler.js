cb60c80fbd442554ebd46376b6a708d541d0859f
Sam <samuel.obrien25@gmail.com>
2019-04-16 11:26:47 -0400

    Much better handling of splash -> dash logic

--- src/Screens/ScreenHandler.js
+++ src/Screens/ScreenHandler.js
@@ -1,28 +1,19 @@
 import React, { useState } from 'react';
 import SplashScreen from './SplashScreen';
 import Dashboard from './Dashboard';
-import styled from 'styled-components';
 import SignInScreen from './SignInScreen';
 import Slide from '../Utilities/Slide';
-
-// #region styles
-const StyledMain = styled.main`
-    position: absolute;
-    top:0;
-    left:0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-`;
-// #endregion styles
 
 function ScreenHandler(props) {
     const [isSplashActive, setIsSplashActive] = useState(true);
     const {activeDatabase, activeUser, isSignedIn} = props;
 
+    //function that waits 5 seconds to let data catch up from Firebase.
+    //Then checks if user props.isSignedIn === true.
+    //If true, render null
+    //If not, render SignInScreen.js
     function authCatchUp(){
         let signInScreen;
-        //One second timeout to let Auth catch up from App.js. Otherwise the sign-in screen will flash.
         setTimeout(() => {
             if(isSignedIn){
                 signInScreen = null;
@@ -30,55 +21,33 @@ function ScreenHandler(props) {
             } else {
                 signInScreen = <SignInScreen isSignedIn={isSignedIn} />
             }
-            return signInScreen
-        }, 1000);
-    }
-
-    /*
-    if(isSignedIn && isSplashActive) {
-
-        setTimeout(() => {
-            setIsSplashActive(false);
+            return signInScreen;
         }, 5000);
-
-        return (
-            <StyledMain>
-                <Slide inOut={isSplashActive ? 'in' : 'out'} animDelay="0s" animDuration="1s" animFillMode="forwards" isForText={false} >
-                    <SplashScreen isSignedIn={isSignedIn} />
-                </Slide>
-            </StyledMain>
-        )
-    } if(isSignedIn && !isSplashActive){
-        return(
-            <StyledMain>
-                <Slide inOut='out' animDelay="0s" animDuration="1s" animFillMode="forwards" isForText={false} >
-                    <SplashScreen isSignedIn={isSignedIn} />
-                </Slide>
-                <Slide inOut='in' animDelay="1s" animDuration="1s" animFillMode="forwards" isForText={false} >
-                    <Dashboard activeUserData={activeUser} activeDatabase={activeDatabase} />
-                </Slide>
-            </StyledMain>
-        )
     }
-    if(!isSignedIn){
-      return (
-          <StyledMain>
-              <Slide inOut={isSplashActive ? 'in' : 'out'} animDelay="0s" animDuration="1s" animFillMode="forwards" isForText={false} >
-                  <SplashScreen isSignedIn={isSignedIn} />
-              </Slide>
-              <SignInScreen isSignedIn={isSignedIn} />
-          </StyledMain>
-      )
-    } 
-}
-*/
 
-    return(
-        <Slide inOut={isSplashActive ? 'in' : 'out'} animDelay="0.25s" animDuration="1s" animFillMode="forwards" isForText={false}>
-            <SplashScreen>
-                {authCatchUp}
-            </SplashScreen>
-        </Slide>
+    //Function that handles the logic for displaying the Dashboard.js.
+    //If isSplashActive state === true, return null.
+    //Else, return Dashboard.js
+    function handleDashboard(){
+        let dashboard =                 
+            <Slide inOut='in' animDelay="0s" animDuration="1s" animFillMode="forwards" isForText={false} fullscreen={true}>
+                   <Dashboard activeUserData={activeUser} activeDatabase={activeDatabase} />
+            </Slide>;
+            if(isSplashActive) return null
+            else return dashboard;
+    }
+
+//Why is this re-rendering FIVE TIMES
+
+console.log(isSignedIn);
+
+    return (
+        <div>
+            <Slide inOut={isSplashActive ? "in" : "out"} animDelay="0s" animDuration="1s" animFillMode="forwards" isForText={false} fullscreen={true}>
+                <SplashScreen> {authCatchUp()}</SplashScreen>
+            </Slide>
+           {handleDashboard()}
+        </div>
     )
 }
 export default ScreenHandler;
