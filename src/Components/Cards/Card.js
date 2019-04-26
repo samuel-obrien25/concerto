91954604c43bee9835450a22658edb7f94327ee1
Sam <samuel.obrien25@gmail.com>
2019-04-26 11:20:15 -0400

    Add proptypes for Card.js

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ThreeDotMenu from '../Buttons/ThreeDotMenu';
 import Action from '../Text/Action';
+import PropTypes from 'prop-types';
 
 //#region Styles
 const StyledCard = styled.div`
     background-color: #fff;
-    height: ${props=>props.isExpanded ? '600px' : 'auto'};
+    height: ${props => props.isExpanded ? '600px' : 'auto'};
     border-radius: 6px;
     box-shadow: 0px 2px 4px rgba(0,0,0,.2);
     transition: .3s ease-in-out;
@@ -46,52 +47,59 @@ const StyledActionContainer = styled.div`
     display: flex;
     position: relative;
 `;
-
 // #endregion
+
 function Card(props) {
 
     const [isExpanded, setIsExpanded] = useState(false);
 
-    function setExpanded(){
+    function setExpanded() {
         setIsExpanded(!isExpanded)
     }
 
-    function getNumberOfConcerts(){
+    function getNumberOfConcerts() {
         const list = props.activeList;
-
         let count;
 
-        if(list.concertList){
+        // Check if list has concerts, set count accordingly
+        if (list.concertList) {
             count = Object.keys(list.concertList).length;
         } else {
             count = 0;
         }
 
-        if(count === 1){
+        if (count === 1) {
             return count + ' concert';
         } else {
             return count + ' concerts'
         }
     }
 
+    const {activeList, deleteList, favoriteList, listTitle, shouldRefresh } = props;
     return (
-        <StyledCard activeList = {props.activeList} isExpanded={isExpanded} shouldRefresh={props.shouldRefresh}>
-        <StyledCardImage />
+        <StyledCard activeList={activeList} isExpanded={isExpanded} shouldRefresh={shouldRefresh}>
+            <StyledCardImage />
             <StyledListTitleContainer>
-                <h2>{props.listTitle}</h2>
+                <h2>{listTitle}</h2>
                 <h3>{getNumberOfConcerts()}</h3>
-                <ThreeDotMenu activeList={props.activeList} favoriteList = {props.favoriteList} deleteList = {props.deleteList}/>
+                <ThreeDotMenu activeList={activeList} favoriteList={favoriteList} deleteList={deleteList} />
             </StyledListTitleContainer>
 
             <StyledActionContainer>
-                <Action text='expand' isExpanded = {isExpanded} handleActionClick={setExpanded} actionIcon='expand'/>
+                <Action text='expand' isExpanded={isExpanded} handleActionClick={setExpanded} actionIcon='expand' />
             </StyledActionContainer>
         </StyledCard>
     );
 }
 
 // #region PROPTYPES
-
+Card.propTypes = {
+    activeList: PropTypes.object,
+    deleteList: PropTypes.func,
+    favoriteList: PropTypes.func,
+    listTitle: PropTypes.string,
+    shouldRefresh: PropTypes.bool
+}
 // #endregion
 
 export default Card;
