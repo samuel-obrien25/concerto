bf22f4c2ae223576e5cb696b39a780ba6da6caee
Sam <samuel.obrien25@gmail.com>
2019-05-21 16:08:29 -0400

    Refactor and add proptypes for modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -2,56 +2,67 @@ import React from 'react';
 import styled from 'styled-components';
 import ListCreator from '../Lists/ListCreator';
 import ConcertCreator from '../Lists/ConcertCreator';
+import PropTypes from 'prop-types';
 
-    const StyledModalWrapper = styled.div`
-        width: 100vw;
-        height: 100vh;
-        background-color: rgba(0,0,0,.5);
-        display: flex;
-        position: fixed;
-        top: 0;
-        left: 0;
-        transition: .25s ease-in-out;
-        opacity: ${props => props.isModalExpanded ? '1' : '0'};
-        transform: ${props => props.isModalExpanded ? 'auto' : 'translateY(100vh)'};
-        z-index: 9999;
-    `;
-    const StyledModal = styled.div`
-        display: block;
-        width: 400px;
-        height: 600px;
-        margin: auto;
-        background-color: white;
-        position: relative;
-    `;
+//#region styles
+const StyledModalWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0,0,0,.5);
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transition: .25s ease-in-out;
+    opacity: ${props => props.isModalExpanded ? '1' : '0'};
+    transform: ${props => props.isModalExpanded ? 'auto' : 'translateY(100vh)'};
+    z-index: 9999;
+`;
+const StyledModal = styled.div`
+    display: block;
+    width: 400px;
+    height: 600px;
+    margin: auto;
+    background-color: white;
+    position: relative;
+`;
+//#endregion
 
 function Modal(props) {
+    const {closeModal, children, handleClick, isModalExpanded, modalType, rawLists, writeConcert, writeList} = props;
+    let modal;
 
-
-    if(props.modalType === 'newList'){
-        return(
-            <StyledModalWrapper isModalExpanded = {props.isModalExpanded} >
-                <StyledModal>
-                    {props.children}
-                    <ListCreator writeList={props.writeList} isActive={props.isModalExpanded} handleClick = {props.handleClick} closeModal = {props.closeModal}/>
-                </StyledModal>
-            </StyledModalWrapper>
-        )
-    }
-    if(props.modalType === 'newConcert') {
-        return (
-            <StyledModalWrapper isModalExpanded = {props.isModalExpanded} >
-                <StyledModal>
-                    {props.children}
-                    <ConcertCreator rawLists = {props.rawLists} writeConcert={props.writeConcert} isActive={props.isModalExpanded} handleClick = {props.handleClick} closeModal = {props.closeModal}/>
-                </StyledModal>
-            </StyledModalWrapper>
-
-        )
+    if(modalType === 'newList'){
+        modal = <ListCreator writeList={writeList} isActive={isModalExpanded} handleClick = {handleClick} closeModal = {closeModal}/>;
     }
 
-    else return null
+    if(modalType === 'newConcert'){
+        modal = <ConcertCreator rawLists = {rawLists} writeConcert={writeConcert} isActive={isModalExpanded} handleClick = {handleClick} closeModal = {closeModal}/>;
+    }
+
+    else {
+        modal = null;
+    }
+
+    return (
+        <StyledModalWrapper isModalExpanded = {isModalExpanded} >
+            <StyledModal>
+                {children}
+                {modal}
+            </StyledModal>
+        </StyledModalWrapper>
+    )
 }
-
+//#region proptypes
+Modal.propTypes = {
+    closeModal: PropTypes.func,
+    children: PropTypes.node,
+    handleClick: PropTypes.func,
+    modalType: PropTypes.string,
+    rawLists: PropTypes.array,
+    writeConcert: PropTypes.func,
+    writeList: PropTypes.func
+}
+//#endregion
 
 export default Modal;
